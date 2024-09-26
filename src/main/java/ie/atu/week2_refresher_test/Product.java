8257938723708fabecd6637a69536dd1783391ce
Cillian Jennings <G00404411@atu.ie>
2024-09-26 13:46:09 +0100

    Finished Homework

--- src/main/java/ie/atu/week2_refresher_test/Product.java
+++ src/main/java/ie/atu/week2_refresher_test/Product.java
@@ -1,5 +1,9 @@
 package ie.atu.week2_refresher_test;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
+    @NotNull
     private long id;
+
+    @NotBlank
+    @Size(min = 3, max=30, message = "Name must be between 3 to 30")
     private String name;
+
+    @PositiveOrZero
     private double price;
 }
