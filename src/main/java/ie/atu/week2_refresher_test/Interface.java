df92953e40e6a270b28b992632b507943bac4ef5
Cillian <G00404411@atu.ie>
2024-10-06 19:04:07 +0100

    Changed a few things so the jdk and dependencies work on my pc at home, probably won't work in college now though.

--- src/main/java/ie/atu/week2_refresher_test/Interface.java
+++ src/main/java/ie/atu/week2_refresher_test/Interface.java
@@ -1,4 +1,7 @@
 package ie.atu.week2_refresher_test;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
 @FeignClient(name = "external-service", url = "http://localhost:8081")
 public interface Interface {
 
